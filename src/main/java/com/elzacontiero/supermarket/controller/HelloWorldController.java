e149521bd4dd1e1a9a0d9e66970f2ac002b89c6d
Elza Contiero <contieroelza@gmail.com>
2022-02-05 20:11:42 +0000

    intial import

---
+++ src/main/java/com/elzacontiero/supermarket/controller/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.elzacontiero.supermarket.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+    @GetMapping("/hi")
+    public ResponseEntity<String> index() {
+        return new ResponseEntity<String>("Hello user!", HttpStatus.OK);
+    }
+
+}
+    
+
+
---
+++ src/test/java/com/elzacontiero/supermarket/SupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elzacontiero.supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
